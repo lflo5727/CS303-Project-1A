de28d8882f853edb3be1182fe52751dd9223c4e2
Liam Floyd <liamf1764@gmail.com>
2018-03-07 15:27:43 -0600

    Setup

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////
+//
+//                  CS303
+//             Od P and Liam F
+//     Project 1A - Infix Expression Parser
+//
+/////////////////////////////////////////////
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+
+}
